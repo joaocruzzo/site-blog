47260949dd55548114683cc30dfab331d27186f0
Joao Pedro Cruz <joaocruzzdev@gmail.com>
2026-02-16 20:12:59 -0300

    add: section hero, feature, support, customer story and call to action

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
-import { Header } from "@/components/header";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { HeroSection } from "@/components/hero-section";
+import { FeatureSection } from "@/components/feature-section";
+import { SupportSection } from "@/components/support-section";
+import { CustomerStorySection } from "@/components/customer-story-section";
+import { CallToAction } from "@/components/call-to-action";
 
 export default function Home() {
   return (
-    <div>
-    </div>
+    <article className="flex flex-col">
+      <HeroSection />
+      <FeatureSection />
+      <SupportSection />
+      <CustomerStorySection />
+      <CallToAction />
+    </article>
   );
 }
