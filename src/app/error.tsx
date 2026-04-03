e996cff4af1ad8a3d48ebfd60d7d7ae11b5c6e39
Joao Pedro Cruz <joaocruzzdev@gmail.com>
2026-04-02 22:11:44 -0300

    add: 404 page, loading suspense and error boundary

---
+++ src/app/error.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { AlertTriangle } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
+type ErrorBoundaryProps = {
+    error: Error
+    reset: () => void
+}
+
+export default function ErrorBoundary({ error, reset }: ErrorBoundaryProps){
+    return (
+        <div className="min-h-[400px] flex items-center justify-center p-4">
+            <div className="max-w-md w-full text-center">
+                <div className="relative inline-block mb-6">
+                    <AlertTriangle
+                    size={64}
+                    className="text-gray-100 mx-auto"
+                    />
+
+                    <div className="absolute h-1 w-16 bg-red-500 -rotate-12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 oopacity-80"></div>
+                </div>
+
+                <h2 className="text-heading-xl text-white font-sans mb-2">
+                    Something went wrong!
+                </h2>
+
+                {error?.message && 
+                    <p className="text-gray-200 mb-2 px-4 text-body-sm overflow-hidden text-ellipsis max-h-24">
+                        {error.message}
+                    </p>
+                }
+
+                <Button variant="primary" className="mt-4" onClick={reset}>
+                    Try again
+                </Button>
+            </div>
+        </div>
+    )
+}
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,15 +1,19 @@
 import { cn } from "@/lib/utils"
 import { CircleX, SearchIcon } from "lucide-react"
 
+import { useCallback, useEffect, useRef } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
-import { useCallback } from "react"
 
 export const Search = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null)
+
     const router = useRouter()
 
     const searchParams = useSearchParams()
 
     const query = searchParams?.get("q") ?? ""
+
+    const hasQuery = !!searchParams?.has("q")
 
     const handleSearch = useCallback((event: React.FormEvent) => {
         event.preventDefault()
@@ -33,6 +37,12 @@ export const Search = () => {
         })
     }
 
+    useEffect(() => {
+        if(hasQuery){
+            inputRef.current?.focus()
+        }
+    }, [hasQuery])
+
     return (
         <form onSubmit={handleSearch} className="relative group w-full md:w-60">
             <SearchIcon className={cn("text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300", query ? "text-blue-300" : "")} />
@@ -40,6 +50,7 @@ export const Search = () => {
             <input
                 type="text"
                 value={query}
+                ref={inputRef}
                 placeholder="Buscar"
                 onChange={handleQueryChange}
                 className="w-full h-10 md:w-60 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
